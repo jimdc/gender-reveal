42a9b71ca32d525bd68c86b5e42b13ddd115827b
James Carroll <jamescarroll@protonmail.ch>
2017-07-04 21:01:32 -0400

    load onready

--- gender-reveal.user.js
+++ gender-reveal.user.js
@@ -2,12 +2,24 @@
 // @name        Gender Reveal
 // @namespace   genderreveal
 // @description Reveal grammatical gender on DuoLingo
-// @include     https://www.duolingo.com/*
-// @version     1
+// @include     https://*.duolingo.com/*
+// @namespace   https://github.com/samideano/gender-reveal
+// @updateURL   https://github.com/samideano/gender-reveal/raw/master/gender-reveal.user.js
+// @version     1.1
 // ==/UserScript==
 
-var words = document.querySelectorAll('[data-test="hint-token"]');
-var i;
-for (i = 0; i < words.length; i++) {
-    words[i].style.backgroundColor = "red";
+function initApplication() {
+    var words = document.querySelectorAll('[data-test="hint-token"]');
+    var i;
+    for (i = 0; i < words.length; i++) {
+        words[i].style.backgroundColor = "red";
+    }
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        initApplication();
+	console.log('hello, world');
+	GM_log('helloGM');
+    }
 }
