f2fcd82af54ae75c271d59a8a65efb7f2297c822
James Carroll <jamescarroll@protonmail.ch>
2018-03-26 16:09:37 -0400

    100ms checks to add color

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gender-reveal
-Userscript to color-code words by grammatical gender on Duolingo to reinforce declension charts
+Color-codes words in [Duolingo](http://www.duolingo.com/) by the grammatical gender shown in its hover-hint. 
+![No hover hint necessary](gender_color_different.png)
+![Info is taken from hover hint, though](gender_in_hint.png)
+
--- gender-reveal.user.js
+++ gender-reveal.user.js
@@ -3,27 +3,77 @@
 // @namespace   genderreveal
 // @description Reveal grammatical gender on DuoLingo
 // @include     https://*.duolingo.com/*
-// @namespace   https://github.com/samideano/gender-reveal
-// @updateURL   https://github.com/samideano/gender-reveal/raw/master/gender-reveal.user.js
-// @version     1.2
+// @namespace   https://github.com/jimdc/gender-reveal
+// @updateURL   https://github.com/jimdc/gender-reveal/raw/master/gender-reveal.user.js
+// @version     1.3
+// @run-at      document-start
 // ==/UserScript==
 
-function highlightWords() {
-    var words = document.querySelectorAll('[data-test="hint-token"]');
-    alert(words);
-    var i;
-    for(i = 0; i < words.length; i++) {
-        words[i].style.backgroundColor = "red";
-    }
-}
+(function() {
 
-function initApplication() {
-    var start = document.querySelectorAll('[data-test="player-next"]');
-    start.addEventListener('click', highlightWords);
-}
+  // React/Duolingo obfuscated class names
+  var classNameExercise = "_1Y5M_"; // Div enclosing all controls of an exercise
+  var nameGenderHint = "td._3s3uv";
+  var nameWord = "span._3_AmQ";
 
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        initApplication();
+  var realConsole = console;
+  var realConsoleLog = console.log;
+  function log(s) {
+    realConsoleLog.call(realConsole, s);
+  }
+
+  function onlyTopText(elemento) {
+    var child = elemento.firstChild;
+    var texts = [];
+    while(child) {
+      if (child.nodeType === 3) {
+        texts.push(child.data);
+      }
+      child = child.nextSibling;
     }
-}
+
+    return texts.join("");
+  }
+
+  function checkDom() {
+    try {
+      if (document.getElementsByClassName(classNameExercise).length !== 1) {
+        return; //No exercise active (<1), or during transition animation between two exercises (>1)
+      }
+      <!-- Todo: find a way not to set the color repeatedly. --> 
+      var words = document.querySelectorAll(nameWord);
+      var i;
+      for(i = 0; i < words.length; i += 1) {        
+        var word = onlyTopText(words[i]);
+        var genderHint = words[i].querySelector(nameGenderHint);
+        if (genderHint !== null) {
+          var gender = genderHint.getElementsByTagName("strong")[0];
+          if (gender !== null) {
+            switch(String(gender.innerHTML)) {
+              case "Masculine":
+                words[i].style.color = "Blue";
+                log("Set blue color to masculine word " + word);
+              break;
+              case "Feminine":
+                words[i].style.color = "Pink";
+                log("Set pink color to feminine word " + word);
+              break;
+              case "Neuter":
+                words[i].style.color = "Green";
+                log("Set green color to neuter word " + word);
+              break;
+            }
+          }
+        }
+
+      }
+
+    } catch (e) {
+      log(e);
+    }
+  }
+
+  setInterval(checkDom, 100);
+  log("Gender reveal loaded");
+
+}) ();
