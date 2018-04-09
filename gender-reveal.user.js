4ba05db38ab9aa83bc83443ac5754fb1bfe96f0d
James Carroll <jamescarroll@protonmail.ch>
2018-04-09 17:30:47 -0400

    make sure this is showing

--- gender-reveal.user.js
+++ gender-reveal.user.js
@@ -7,12 +7,12 @@
 // @require     color-chooser.js
 // @namespace   https://github.com/jimdc/gender-reveal
 // @updateURL   https://github.com/jimdc/gender-reveal/raw/master/gender-reveal.user.js
-// @version     0.6
+// @version     0.7
 // @run-at      document-start
 // ==/UserScript==
 
 (function() {
-  const version = 0.6
+  const version = 0.7
 
   // React/Duolingo obfuscated class names
   const classNameExercise = "_1Y5M_"; // Div enclosing all controls of an exercise
